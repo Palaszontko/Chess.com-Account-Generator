7572e9cf5ea7b447fa6ab3b1be1ef4ba1b475af4
Olgierd Palasz <olgierd.palasz@gmail.com>
2024-03-23 19:16:17 +0100

    fix: change csv data separator

--- src/modules/collect_data_csv.ts
+++ src/modules/collect_data_csv.ts
@@ -2,7 +2,8 @@ import fs from 'fs';
 import { logger } from './logger';
 
 export function append_user(user: any, path: string) {
-  const data = '\n' + [user.date, user.nickname, user.password, user.game_link, user.captcha].join(';');
+  const data =
+    '\n' + [user.date, user.nickname, user.password, user.game_link, user.captcha].join(',');
   fs.appendFile(path, data, (response: any) => {
     logger.debug('User data saved.');
   });
